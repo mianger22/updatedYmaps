5d012ce60119883cf7134d440b39bddae9059fd2
BossNumber1 <dimaang23@gmail.com>
2023-11-04 10:35:43 +0300

    Added repeated marking of a friend and a change

--- src/components/YandexMap.tsx
+++ src/components/YandexMap.tsx
@@ -2,19 +2,29 @@ import { useState, useEffect } from 'react';
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
 
 const YandexMap = () => {
-  const [currentCoordinates, setCoordinates] = useState([0, 0]);
+  const [myCoordinates, setMyCoordinates] = useState([0, 0]);
+  const [friendCoordinates, setFriendCoordinates] = useState([0, 0]);
+
+  const changeFriendCoordinates = (e: any) => {
+    e.preventDefault();
+    setFriendCoordinates(e.get('coords'));
+  };
 
   useEffect(() => {
     // получаю местоположение пользователя и сохраняю его
     navigator.geolocation.getCurrentPosition((position) => {
-      setCoordinates([+position.coords.latitude, +position.coords.longitude]);
+      setMyCoordinates([+position.coords.latitude, +position.coords.longitude]);
     });
   }, []);
 
   return (
     <YMaps>
-      <Map defaultState={{ center: currentCoordinates, zoom: 17 }}>
-        <Placemark defaultGeometry={currentCoordinates} />
+      <Map 
+        defaultState={{ center: myCoordinates, zoom: 17 }}
+        onClick={changeFriendCoordinates}
+      >
+        <Placemark geometry={myCoordinates} />
+        {friendCoordinates.toString() !== [0, 0].toString() && <Placemark geometry={friendCoordinates} />}
       </Map>
     </YMaps>
   )
