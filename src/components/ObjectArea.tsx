654bedc5dc1fd441c9d7ef0c6c36c4d939bf754a
BossNumber1 <dimaang23@gmail.com>
2023-11-09 18:18:46 +0300

    сделал балун у круга, как у иконки

--- src/components/ObjectArea.tsx
+++ src/components/ObjectArea.tsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import { Circle } from 'react-yandex-maps';
 import { ObjectProps, PlaceInformationType } from './types/CommonTypes';
 
 function ObjectArea(props: ObjectProps) {
   const object_data: PlaceInformationType = props.data_place;
+  const [arrived, setArrived] = useState(true);
   
   return (
     <Circle
         geometry={[object_data.location_coordinates, 2]}
         properties={{
           hintContent: object_data.name_of_place,
-          balloonContent: object_data.name_of_place,
+          balloonContentHeader: object_data.name_of_place,
+          balloonContentBody: `
+            <div className="driver-card">
+              Ресурс: <b>${object_data.received_resource}</b><br>
+              Производительность: <b>${object_data.capacity} единиц в минуту</b><br>
+            </div>`,
+          balloonContentFooter: arrived && `<div onclick="alert('Захвачена позиция ${object_data.name_of_place}!')">Захватить позицию</div>`
           // iconContent: data_place.name_of_place[0],
         }}
         options={{
